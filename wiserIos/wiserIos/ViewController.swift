a38f3b8523f05c3d5dcee841fbc7f81f45dbaf07
Pehj <peterhj@gmail.com>
2015-08-26 20:49:51 +0200

    Map displays pin

--- wiserIos/wiserIos/ViewController.swift
+++ wiserIos/wiserIos/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     //Properties
     @IBOutlet var mapView: MKMapView!
@@ -21,26 +21,60 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         //Setup location
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        //Map delegation for pin behaviour
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    //Location
+    //MKMapViewDelegate
+    //http://stackoverflow.com/questions/24523702/stuck-on-using-mkpinannotationview-within-swift-and-mapkit/24532551#24532551
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if let newestAnnotation = mapView.annotations.first {
+            mapView.removeAnnotations(mapView.annotations)
+            mapView.addAnnotation(newestAnnotation)
+        }
+        
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.animatesDrop = true
+            pinView!.pinTintColor = UIColor.purpleColor()
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    //CLLocationManagerDelegate
     let locationManager = CLLocationManager()
-
+    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = manager.location {
-            
             let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            mapView.region = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
+            mapView.region = MKCoordinateRegionMakeWithDistance(coordinate, 1000, 1000)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            
+            mapView.addAnnotation(annotation)
         }
         var pm = [CLPlacemark]()
         CLGeocoder().reverseGeocodeLocation(locations[0], completionHandler: {
