42ef8389b668f71fc0646fba4da430de3b809ff1
Nikolaj Holden <nikolajholden@gmail.com>
2015-08-27 13:19:13 +0200

    Moved javascript to individual files and created initial mockup of Room page for web application.

--- WisR/WisR/App_Start/BundleConfig.cs
+++ WisR/WisR/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace WisR
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/googleApi.js", "~/Scripts/geolocationScripts.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ WisR/WisR/Scripts/geolocationScripts.js
@@ -0,0 +1,83 @@
+﻿$(document).ready(function () {
+    //toggleLocation();
+});
+
+
+
+
+//Code for geolocation
+var map;
+var initialLoad = true;
+var marker;
+var positionId;
+var currentLocation;
+var geocoder = new google.maps.Geocoder();
+
+function moveMap(position) {
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    //Remove previous marker
+    if (marker != null) {
+        marker.setMap(null);
+    }
+    marker = new google.maps.Marker({
+        position: latLng
+    });
+    marker.setMap(map);
+    //map.addOverlay(marker);
+    map.setZoom(15);
+    map.panTo(latLng);
+    $("#accuracy").html('Precision: ' + position.coords.accuracy + ' meters');
+
+    //Hide map on first load
+    /*if (initialLoad) {
+        toggleLocation();
+        initialLoad = false;
+    }*/
+    
+    //Get address from location
+    geocoder.geocode({ 'location': latLng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if (results[1]) {
+                $("#address").html('You are here: ' + results[1].formatted_address);
+
+
+            } else {
+                window.alert('No results found');
+            }
+        } else {
+            window.alert('Geocoder failed due to: ' + status);
+        }
+    });
+
+}
+
+function handleError(error) { alert(error.message); }
+
+function initializeGeolocation() {
+
+    var mapOptions = {
+        disableDefaultUI: true
+    }
+    var positionOptions = {
+        enableHighAccuracy: true
+    };
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+    positionId = navigator.geolocation.watchPosition(moveMap, handleError, positionOptions);
+    google.maps.event.addDomListener(window, "resize", function () {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+}
+// Code for hiding google maps 
+
+function toggleLocation() {
+    $("#googlemaps").toggle();
+    initializeGeolocation();
+    /*var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center);*/
+
+}
