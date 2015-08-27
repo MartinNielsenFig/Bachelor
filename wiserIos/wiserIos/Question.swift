0564cd11c9f0b4c12d9b825b7aebbd897f051ec2
Pehj <peterhj@gmail.com>
2015-08-27 13:31:15 +0200

    question iOS added

---
+++ wiserIos/wiserIos/Question.swift
@@ -0,0 +1,35 @@
+//
+//  Question.swift
+//  wiserIos
+//
+//  Created by Peter Helstrup Jensen on 27/08/2015.
+//  Copyright © 2015 Peter Helstrup Jensen. All rights reserved.
+//
+
+import Foundation
+
+enum QuestionType {
+    case Boolean
+    case MultipleChoice
+    case Textuel
+    case BrainStorming
+}
+
+class Question {
+    
+    var id: String?
+    var questionType: QuestionType
+    var questionText: String
+    var responseOptions: [ResponseOption]?
+    var img: String?
+    var result: [Answer]?
+    var upVotes: Int = 0
+    var downVotes: Int = 0
+    var createdById: String
+    
+    init(questionType: QuestionType, questionText: String, createdById: String) {
+        self.questionType = questionType
+        self.questionText = questionText
+        self.createdById = createdById
+    }
+}
