dea498b2f41b3fc4fa79de6a00dfb9cf488a169f
aceipse <martin.fig@hotmail.com>
2015-09-01 14:57:04 +0200

    Fixed after merge problem

--- WisR/WisR/Scripts/AngularHomeScripts.js
+++ WisR/WisR/Scripts/AngularHomeScripts.js
@@ -5,7 +5,13 @@ app.config(['$httpProvider', function ($httpProvider) {
 }]);
 
 app.controller("HomeController", ['$scope', '$http', function ($scope, $http) {
-    $scope.title = 'Martin sucks';
+    var getRooms = function() {
+        $http.post('http://localhost:1337/Room/GetAll').then(function(response) {
+            $scope.Rooms = response.data;
+        });
+    };
+    getRooms();
+    $scope.title = 'Room name';
     $scope.RoomName = "";
     $scope.Radius = 2;
     $scope.UniqueTag = "";
@@ -13,22 +19,10 @@ app.controller("HomeController", ['$scope', '$http', function ($scope, $http) {
     $scope.HasChat = true;
     $scope.UserCanAsk = true;
     $scope.AllowAnonymous = true;
+
+ 
     $scope.postRoom = function () {
         //Make get request for json object conversion
-        $http.post('/Home/toJsonRoom', { RoomName: $scope.RoomName, CreatedBy: $scope.CreatedBy, location: currentLocation, radius: $scope.Radius, tag: $scope.UniqueTag, password: $scope.Password, hasChat: $scope.HasChat, userCanAsk: $scope.UserCanAsk, allowAnonymous: $scope.AllowAnonymous }).
-  then(function (response) {
-
-      //Use response to send to REST API
-      $http.post('http://localhost:1337/Room/CreateRoom', { Room: JSON.stringify(response.data) }).
-  then(function (response) {
-
-  }, function (response) {
-
-  });
-
-  }, function (response) {
-
-  });
         $http.post('/Home/toJsonRoom',
         {
             RoomName: $scope.RoomName,
