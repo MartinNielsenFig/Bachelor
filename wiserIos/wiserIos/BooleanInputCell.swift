68c8cb2804945fe98b2f6b7be1ce24609d2f5db6
Pehj <peterhj@gmail.com>
2015-09-03 12:11:55 +0200

    BooleanInput

---
+++ wiserIos/wiserIos/BooleanInputCell.swift
@@ -0,0 +1,26 @@
+//
+//  BooleanInputCell.swift
+//  wiserIos
+//
+//  Created by Peter Helstrup Jensen on 03/09/2015.
+//  Copyright © 2015 Peter Helstrup Jensen. All rights reserved.
+//
+
+import UIKit
+
+class BooleanInputCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var uiSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
