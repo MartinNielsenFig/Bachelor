ee15aac3de663fbbbf73d363efe52bc1f22e4f4d
TomasFrithjofsson <tomasfrithjofsson@gmail.com>
2015-09-03 08:42:26 +0200

    HTTP Request...

--- WisRAndroid/app/src/main/java/com/example/tomas/wisrandroid/Activities/CreateRoomActivity.java
+++ WisRAndroid/app/src/main/java/com/example/tomas/wisrandroid/Activities/CreateRoomActivity.java
@@ -2,20 +2,74 @@ package com.example.tomas.wisrandroid.Activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.tomas.wisrandroid.Helpers.ActivityLayoutHelper;
+import com.example.tomas.wisrandroid.Model.Room;
 import com.example.tomas.wisrandroid.R;
+import com.google.gson.Gson;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class CreateRoomActivity extends ActionBarActivity {
+
+    Button mTestKnap;
+    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_room);
-
         ActivityLayoutHelper.HideLayout(getWindow(), getSupportActionBar());
+
+        final TextView mTextView= (TextView) findViewById(R.id.create_room_textview);
+
+        mTestKnap = (Button)findViewById(R.id.test_knap);
+        mTestKnap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(CreateRoomActivity.this);
+                String url ="http://10.0.2.2:1337/Room/CreateRoom/";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                mTextView.setText("Response is: "+ response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mTextView.setText("That didn't work!");
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
+            }
+        });
     }
 
     @Override
@@ -38,5 +92,22 @@ public class CreateRoomActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String getPostData(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 }
