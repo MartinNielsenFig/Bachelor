f324865763dc75b44009ce17179a5c351e30e1fb
Pehj <peterhj@gmail.com>
2015-09-07 14:29:32 +0200

    No reverse geolocation. Stop cont. update location

--- wiserIos/wiserIos/ChooseRoleViewController.swift
+++ wiserIos/wiserIos/ChooseRoleViewController.swift
@@ -30,17 +30,6 @@ class ChooseRoleViewController: UIViewController, CLLocationManagerDelegate, MKM
         
         //Map delegation for pin behaviour
         mapView.delegate = self
-        
-        
-        //Question text 
-        /*
-        let question = BooleanQuestion()
-        question.QuestionText = "ushtest1"
-        question._id = nil
-        let qJson = JSONSerializer.toJson(question)
-        print(qJson)
-        HttpHandler.createQuestion("doge", question: qJson, type: "BooleanQuestion")
-        */
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,15 +39,6 @@ class ChooseRoleViewController: UIViewController, CLLocationManagerDelegate, MKM
     //MKMapViewDelegate
     //http://stackoverflow.com/questions/24523702/stuck-on-using-mkpinannotationview-within-swift-and-mapkit/24532551#24532551
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        if let newestAnnotation = mapView.annotations.first {
-            mapView.removeAnnotations(mapView.annotations)
-            mapView.addAnnotation(newestAnnotation)
-        }
-        
-        if annotation is MKUserLocation {
-            return nil
-        }
-        
         let reuseId = "pin"
         var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
         if pinView == nil {
@@ -71,6 +51,7 @@ class ChooseRoleViewController: UIViewController, CLLocationManagerDelegate, MKM
             pinView!.annotation = annotation
         }
         
+        locationManager.stopUpdatingLocation()
         return pinView
     }
     
@@ -78,28 +59,16 @@ class ChooseRoleViewController: UIViewController, CLLocationManagerDelegate, MKM
     let locationManager = CLLocationManager()
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("did update location")
+        
         if let location = manager.location {
             let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
             mapView.region = MKCoordinateRegionMakeWithDistance(coordinate, 1000, 1000)
             
             let annotation = MKPointAnnotation()
             annotation.coordinate = coordinate
-            
             mapView.addAnnotation(annotation)
         }
-        var pm = [CLPlacemark]()
-        CLGeocoder().reverseGeocodeLocation(locations[0], completionHandler: {
-            (placemarks, error) in
-                if (error != nil) {
-                    print("reverse geodcode fail: \(error!.localizedDescription)")
-                }
-            if placemarks != nil {
-                pm = placemarks as [CLPlacemark]!
-            }
-        })
-        if pm.count <= 0 { return }
-        print(pm[0].country)
-        print(pm[0].postalCode)
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
