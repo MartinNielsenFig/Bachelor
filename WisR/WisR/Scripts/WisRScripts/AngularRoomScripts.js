85cc51d55aebd7652d533aed64f348338dd515ea
Nikolaj Holden <nikolajholden@gmail.com>
2015-09-08 10:19:33 +0200

    Hides divs and disables buttons based on room configuration

--- WisR/WisR/Scripts/WisRScripts/AngularRoomScripts.js
+++ WisR/WisR/Scripts/WisRScripts/AngularRoomScripts.js
@@ -1,9 +1,28 @@
 ﻿app.controller("RoomController", [
-    '$scope', '$http', 'configs', function($scope, $http, configs) {
+    '$scope', '$http', 'configs', '$window', function ($scope, $http, configs, $window) {
+
+        //Button init function
+        var initButton=function() {           
+                $("#createQuestionButton").popover({ placement: 'bottom', html: true });
+                $("#createQuestionDiv").hover(function () {
+                    if ($('#createQuestionButton').prop("disabled")) {
+                        $("#createQuestionButton").popover('toggle');
+                    }
+                });
+        }
+        initButton();
+        //watch the window.userId variable
+        $scope.$watch(
+                function () {
+                    return $window.userId;
+                 }, function (n, o) {
+                $scope.userId = n;
+            }
+);
 
         //Get all questions
-        var getQuestions = function() {
-            $http.get(configs.restHostName + '/Question/GetAll').then(function(response) {
+        var getQuestions = function () {
+            $http.get(configs.restHostName + '/Question/GetAll').then(function (response) {
                 $scope.Questions = response.data;
             });
         };
@@ -12,12 +31,12 @@
         //Get information about this specific room
         //Get all questions
         var getRoom = function () {
-            $http.post(configs.restHostName + '/Room/GetById',{id:MyRoomIdFromViewBag}).then(function (response) {
+            $http.post(configs.restHostName + '/Room/GetById', { id: MyRoomIdFromViewBag }).then(function (response) {
                 $scope.CurrentRoom = response.data;
             });
         };
         getRoom();
-
+        $scope.userIsHost = false;
         $scope.SpecificQuestionShown = false;
         $scope.QuestionText = "Enter question";
         $scope.QuestionType = "TextualQuestion";
@@ -25,20 +44,19 @@
         $scope.Picture = "Add a picture";
 
         //function for showing a specific question
-        $scope.ShowSpecificQuestion = function(question) {
+        $scope.ShowSpecificQuestion = function (question) {
             $scope.ToggleShowQuestionTables();
             $scope.SpecificQuestion = question;
         }
-        $scope.ToggleShowQuestionTables = function() {
+        $scope.ToggleShowQuestionTables = function () {
             $scope.SpecificQuestionShown = !$scope.SpecificQuestionShown;
         }
 
-
         //Function for creating a question
-        $scope.postQuestion = function() {
+        $scope.postQuestion = function () {
             //Make get request for json object conversion
-            $http.post('/Room/toJsonQuestion', { CreatedBy: window.userId, RoomId: MyRoomIdFromViewBag, Downvotes: 0, Image: $scope.Picture, Upvotes: 0, QuestionText: $scope.QuestionText }).
-                then(function(response) {
+            $http.post('/Room/toJsonQuestion', { CreatedBy: $window.userId, RoomId: MyRoomIdFromViewBag, Downvotes: 0, Image: $scope.Picture, Upvotes: 0, QuestionText: $scope.QuestionText }).
+                then(function (response) {
                     //Use response to send to REST API
                     $http.post(configs.restHostName + '/Question/CreateQuestion', { question: JSON.stringify(response.data), type: $scope.QuestionType });
                 });
@@ -50,10 +68,10 @@
             $scope.textAreaModel = "";
 
             //Make get request for json object conversion
-            $http.post('/Room/toJsonChatMessage', { userId:window.userId,roomId:MyRoomIdFromViewBag,text: message}).
+            $http.post('/Room/toJsonChatMessage', { userId: window.userId, roomId: MyRoomIdFromViewBag, text: message }).
                 then(function (response) {
                     //Use response to send to REST API
-                    $http.post(configs.restHostName + '/Chat/CreateChatMessage', { ChatMessage: JSON.stringify(response.data)});
+                    $http.post(configs.restHostName + '/Chat/CreateChatMessage', { ChatMessage: JSON.stringify(response.data) });
                 });
         }
     }
