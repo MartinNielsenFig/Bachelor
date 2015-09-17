220fdd67ec215e2a9b6dddbc430211a5ac31db15
Pehj <peterhj@gmail.com>
2015-09-17 16:22:58 +0200

    Revert JSQMessagesViewController to old

--- wiserIos/wiserIos/ChatViewController.swift
+++ wiserIos/wiserIos/ChatViewController.swift
@@ -7,93 +7,77 @@
 //
 
 import UIKit
-import JSQMessagesViewController
 
-class ChatViewController: JSQMessagesViewController, UITextFieldDelegate, Paged {
+class ChatViewController: UIViewController, UITextFieldDelegate, Paged {
+    let pageIndex = 2
+    var roomId: String?
     
-    //JSQMessagesViewController
-    //Framework by jessesquires https://github.com/jessesquires/JSQMessagesViewController
-    //Tutorial inspired by https://www.syncano.io/ios-chat-app-jsqmessagesviewcontroller/
-    var userName = String()
-    var messages = [JSQMessage]() {
+    var chat = String() {
         didSet {
-            dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                self.collectionView!.reloadData()
+            dispatch_async(dispatch_get_main_queue()) {
+                self.chatTextField.text = self.chat
+                let range = NSMakeRange(self.chatTextField.text.characters.count - 1, 1)
+                self.chatTextField.scrollRangeToVisible(range)
             }
         }
     }
     
-    let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImageWithColor(UIColor(red: 231/255, green: 231/255, blue: 233/255, alpha: 1.0))!
-    let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImageWithColor(UIColor(red: 0, green: 122/255, blue: 1, alpha: 1.0))!
+    @IBOutlet weak var chatTextField: UITextView!
+    @IBOutlet weak var chatMessageInput: UITextField!
     
-    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
-        let data = self.messages[indexPath.row]
-        return data
-    }
-    
-    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
-        let data = self.messages[indexPath.row]
-        if (data.senderId == self.senderId) {
-            return self.outgoingBubble
-        } else {
-            return self.incomingBubble
+    override func viewDidLoad() {
+        chatMessageInput.delegate = self
+        
+        HttpHandler.getChatMessages(roomId!) { (messages) -> Void in
+            var tempChat = String()
+            for m in messages {
+                let line = DateTimeHelper.getTimeStringFromEpochString(m.Timestamp) + " " + m.Value! + "\n"
+                tempChat += line
+            }
+            self.chat = tempChat
         }
+        
+        super.viewDidLoad()
     }
     
-    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
-        return nil
-    }
-    
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.messages.count
-    }
-
-    /*override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = self.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
-        
-        cell.textView!.textColor = UIColor.blackColor()
-        return cell
-    }*/
-        
-    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
-        let newJsqMessage = JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text)
-        messages += [newJsqMessage]
+    //UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if let text = textField.text where text.isEmpty {
+            return false
+        }
         
         let msg = ChatMessage()
         msg.ByUserId = CurrentUser.sharedInstance._id
         msg.RoomId = roomId
         //message timestamp gets created on restApi
-        msg.Value = newJsqMessage.text
+        msg.Value = textField.text
         
         HttpHandler.sendChatMessage(JSONSerializer.toJson(msg))
+        
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        movePlate(textField, up: true)
+    }
+    func textFieldDidEndEditing(textField: UITextField) {
+        movePlate(textField, up: false)
+    }
 
-        self.finishSendingMessage()
+    
+    //http://stackoverflow.com/questions/1247113/iphone-keyboard-covers-uitextfield
+    func movePlate(field: UITextField, up: Bool) {
+        
+        let movementDistance = 220 // tweak as needed
+        let movementDuration = 0.3 // tweak as needed
+        
+        let movement = (up ? -movementDistance : movementDistance)
+        
+        UIView.beginAnimations("anim", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(NSTimeInterval(movementDuration))
+        self.view.frame = CGRectOffset(self.view.frame, 0, CGFloat(movement))
+        UIView.commitAnimations()
     }
     
-    override func didPressAccessoryButton(sender: UIButton!) {
-    }
-    
-    //Paged
-    let pageIndex = 2
-    var roomId: String?
-    
-    override func viewDidLoad() {
-        
-        self.userName = "iPhone"
-        self.collectionView!.reloadData()
-        self.senderDisplayName = self.userName
-        self.senderId = self.userName
-        
-        HttpHandler.getChatMessages(roomId!) { (messages) -> Void in
-            var tmpMessages = [JSQMessage]()
-            for m in messages {
-                let date = DateTimeHelper.getDateFromEpochString(m.Timestamp)
-                tmpMessages += [JSQMessage(senderId: m.ByUserId ?? self.userName, senderDisplayName: "StillNeedFetch", date: date, text: m.Value)]
-            }
-            
-            self.messages += tmpMessages
-        }
-        
-        super.viewDidLoad()
-    }
 }
