176906725c43ea36dfab39575cd2629e4cb6d117
aceipse <martin.fig@hotmail.com>
2015-10-01 13:22:37 +0200

    Done some cleanup in homecontroller

---
+++ WisR/WisR/Scripts/WisRScripts/AngularModule.js
@@ -0,0 +1,6 @@
+﻿//Creates the angular module, which include:
+//  naif.base64 for use on base64 imaging,
+//  luegg.directives for use on chat
+//  chart.js to make the charts
+var app = angular.module("wisrApp", ['naif.base64', 'luegg.directives', "chart.js"]);
+
---
+++ WisR/WisR/Scripts/WisRScripts/AngularUserScripts.js
@@ -0,0 +1,3 @@
+﻿//Has to be defined here, to be able to use it in _Layout.cshtml
+app.controller("UserController", ['$scope', function ($scope) {
+}]);
