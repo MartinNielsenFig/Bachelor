0aef652bac499d684d8f3d7438a3426a36924d0f
peheje <peterhj@gmail.com>
2015-10-07 12:01:52 +0200

    Added error codes, and rabbit mq handling in CreateRoom

---
+++ WisRRestAPI/WisRRestAPI/ErrorCodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WisRRestAPI {
+
+    //Use describing names.
+    public enum ErrorCodes {
+        RoomTagAlreadyInUse,
+        RabbitMqError,
+        StringIsNotJsonFormat,
+        CouldNotParseJsonToClass
+    }
+}
