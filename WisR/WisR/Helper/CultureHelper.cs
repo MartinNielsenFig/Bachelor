686d4f0e922c13935737b5518a6464d6df41faa7
Nikolaj Holden <nikolajholden@gmail.com>
2015-10-22 08:18:34 +0200

    Added culturehelper class

---
+++ WisR/WisR/Helper/CultureHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using System.Threading;
+using System.Globalization;
+
+namespace WisR.Helper
+{
+
+    /// <summary>
+    /// Class found at: http://www.c-sharpcorner.com/UploadFile/4d9083/globalization-and-localization-in-Asp-Net-mvc-4/
+    /// </summary>
+    public class CultureHelper
+    {
+        protected HttpSessionState session;
+
+        //constructor   
+        public CultureHelper(HttpSessionState httpSessionState)
+        {
+            session = httpSessionState;
+        }
+        // Properties  
+        public static int CurrentCulture
+        {
+            get
+            {
+                if (Thread.CurrentThread.CurrentUICulture.Name == "en-GB")
+                {
+                    return 0;
+                }
+                else if (Thread.CurrentThread.CurrentUICulture.Name == "da-DK")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+
+                if (value == 0)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
+                }
+                else if (value == 1)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("da-DK");
+                }
+                else
+                {
+                    Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+                }
+
+                Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+
+            }
+        }
+    }
+}
