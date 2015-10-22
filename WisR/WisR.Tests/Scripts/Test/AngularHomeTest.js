6cb11b9bc05647a6e4005de565dd97da2908caef
aceipse <martin.fig@hotmail.com>
2015-10-22 11:11:23 +0200

    Test and changed so empty password does not get encrypted

--- WisR/WisR.Tests/Scripts/Test/AngularHomeTest.js
+++ WisR/WisR.Tests/Scripts/Test/AngularHomeTest.js
@@ -1,4 +1,5 @@
 ﻿///<reference path="~/Scripts/jasmine/jasmine.js"/>
+///<reference path="~/../WisR/Scripts/CryptoJS/rollups/sha512.js"/>
 ///<reference path="~/../WisR/Scripts/jquery-1.11.3.js"/>
 ///<reference path="~/../WisR/Scripts/jquery.signalR-2.2.0.js"/>
 ///<reference path="~/../WisR/Scripts/angular.js"/>
@@ -22,11 +23,28 @@ describe("Homecontroller", function () {
 
     describe("Home controller", function () {
 
-        var scope, controller;
+        var scope, controller, httpBackend, config;
 
-        beforeEach(inject(function ($rootScope, $controller) {
+        beforeEach(inject(function ($rootScope, $controller, $httpBackend, configs) {
             scope = $rootScope.$new();
+            config = configs;
+            httpBackend = $httpBackend;
 
+            //Setup for http request
+            httpBackend.when('GET', 'http://localhost:1337/Room/GetAll').respond({});
+            httpBackend.when('POST', 'http://localhost:7331/Home/toJsonRoom').respond({});
+            httpBackend.when('POST', 'http://localhost:1337/Room/CreateRoom').respond({});
+            httpBackend.when('POST', 'http://localhost:7331/Home/toJsonUser').respond({});
+            httpBackend.when('POST', 'http://localhost:1337/User/UpdateUser').respond({});
+           
+
+            //Setup for currentUser
+            scope.currentUser = {ConnectedRoomIds: [] }
+
+            //Setup for location
+            scope.currentLocation = { coords: { latitude: 1 } };
+            scope.currentLocation = { coords: { longitude: 1 } };
+            
             controller = $controller('HomeController', { $scope: scope });
         }));
 
@@ -47,10 +65,62 @@ describe("Homecontroller", function () {
         });
     
         it('should change location to ?RoomId=1', function () {
-            var room = { _id: 1 }
-            scope.userId = 'NoUser';
+            var room = { _id: 1, AllowAnonymous: true }
+            scope.userId = 'Martin';
             scope.changeViewToRoom(room);
-            expect(location.href).toBe('http://localhost:63385/?RoomId=1');
+            expect(scope.RoomId).toBe(1);
+        });
+
+        it('should give call GetAll', function() {
+            httpBackend.expectGET('http://localhost:1337/Room/GetAll');
+            scope.getRooms();
+            httpBackend.flush();
+        });
+
+        it('should call toJsonRoom, CreateRoom, toJsonUser and UpdateUser', function () {
+            httpBackend.expectPOST('http://localhost:7331/Home/toJsonRoom');
+            httpBackend.expectPOST('http://localhost:1337/Room/CreateRoom');
+            httpBackend.expectPOST('http://localhost:7331/Home/toJsonUser');
+            httpBackend.expectPOST('http://localhost:1337/User/UpdateUser');
+            scope.postRoom();
+            httpBackend.flush();
+        });
+
+        it('password should have been encrypted', function() {
+            scope.Password = "Martin";
+            scope.postRoom();
+            expect(scope.HashedPassword.length === 128);
+        });
+
+        it('password shouldn\'t have been encrypted', function () {
+            scope.postRoom();
+            expect(scope.HashedPassword === undefined);
+        });
+
+        it('changeViewToRoom should have been called', function () {
+            httpBackend.when('POST', 'http://localhost:1337/Room/GetByUniqueTag').respond({ data: { _id: 1 } });
+            httpBackend.expectPOST('http://localhost:1337/Room/GetByUniqueTag');
+            spyOn(scope, 'changeViewToRoom');
+            scope.connectWithUniqueTag();
+            expect(scope.changeViewToRoom).toHaveBeenCalled();
+            httpBackend.flush();
+        });
+
+        it('message should be "No room with the tag: 20"', function () {
+            httpBackend.when('POST', 'http://localhost:1337/Room/GetByUniqueTag').respond({ data: {_id: undefined} });
+            httpBackend.expectPOST('http://localhost:1337/Room/GetByUniqueTag');
+            scope.uniqueRoomTag = 20;
+
+            scope.connectWithUniqueTag();
+            expect(scope.Message).toBe("No room with the tag: 20");
+            httpBackend.flush();
+        });
+
+        it('should call GetByUniqueTag', function () {
+            httpBackend.when('POST', 'http://localhost:1337/Room/GetByUniqueTag').respond({ data: { _id: 1 } });
+            httpBackend.expectPOST('http://localhost:1337/Room/GetByUniqueTag');
+            scope.connectWithUniqueTag();
+            httpBackend.flush();
         });
     });
 });
