5e36cf93e7db4ace83b8d586773d92d22ed5afa3
Pehj <peterhj@gmail.com>
2015-10-30 14:59:31 +0100

    Create question is now modal window

--- WisRiOS/wiserIos/Assets.xcassets/CreateRoomViewController.swift
+++ WisRiOS/wiserIos/Assets.xcassets/CreateRoomViewController.swift
@@ -34,6 +34,8 @@ class CreateRoomViewController: UITableViewController {
         super.viewDidLoad()
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "addRoomButtonPressed:")
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: "dismiss")
+        
     }
     
     //MARK: Tableview
@@ -145,6 +147,11 @@ class CreateRoomViewController: UITableViewController {
     }
     
     //MARK: Utilities
+    
+    func dismiss() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     func enablePwSwitchChanged(uiSwitch: UISwitch) {
         pwInputCell?.inputField.enabled = uiSwitch.on
         pwLabel?.enabled = uiSwitch.on
