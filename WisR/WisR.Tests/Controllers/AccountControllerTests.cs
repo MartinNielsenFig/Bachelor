20fa1e12baeead961b41ea779a2147424a624572
Nikolaj Holden <nikolajholden@gmail.com>
2015-11-04 14:38:50 +0100

    Added test files

---
+++ WisR/WisR.Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WisR.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WisR.Controllers.Tests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        [TestMethod()]
+        public void AccountControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AccountControllerTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoginTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VerifyCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VerifyCodeTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConfirmEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ForgotPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ForgotPasswordTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ForgotPasswordConfirmationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetPasswordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetPasswordTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetPasswordConfirmationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExternalLoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendCodeTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExternalLoginCallbackTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExternalLoginConfirmationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LogOffTest()
+        {
+Assert.Fail();
+        }
+    }
+}
---
+++ WisR/WisR.Tests/Controllers/RoomControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WisR.Providers;
+
+namespace WisR.Controllers.Tests
+{
+    [TestClass]
+    public class RoomControllerTests
+    {
+        [TestMethod]
+        public void toJsonChatMessageTest()
+        {
+            //Arrange
+            var rabbitMock = new Mock<IRabbitSubscriber>();
+            var controller = new RoomController(rabbitMock.Object);
+            var userid = "Test user id";
+            var roomId = "Test room id";
+            var text = "Test text";
+
+
+            //Act
+            var testChatMsg = controller.toJsonChatMessage(userid, roomId, text);
+
+            //Assert - this one excludes timestamp since it is time based and will always fail
+            Assert.IsTrue(
+                testChatMsg.Contains(
+                    "{ \"_id\" : null, \"ByUserId\" : \"Test user id\", \"RoomId\" : \"Test room id\", \"Value\" : \"Test text\","));
+        }
+
+        [TestMethod]
+        public void toJsonQuestionTestWithNullValues()
+        {
+            //Arrange
+            var rabbitMock = new Mock<IRabbitSubscriber>();
+            var controller = new RoomController(rabbitMock.Object);
+            var createdBy = "Test user";
+            var roomId = "Test room id";
+            var image = "Test image";
+            var questionText = "Test question text";
+            string responseOptions = null;
+            string questionResult = null;
+            var CreationTimeStamp = "Test timestamp";
+            var ExpireTimeStamp = "Test expire";
+            var questionType = "Test Question Type";
+            string votes = null;
+
+
+            //Act
+            var testQuestionMsg = controller.toJsonQuestion(createdBy, roomId, image, questionText, responseOptions,
+                questionResult, CreationTimeStamp, ExpireTimeStamp, questionType, votes);
+
+            //Assert
+            Assert.AreEqual(
+                "{ \"_id\" : null, \"RoomId\" : \"Test room id\", \"CreatedById\" : \"Test user\", \"Votes\" : [], \"Img\" : \"Test image\", \"QuestionText\" : \"Test question text\", \"ResponseOptions\" : [], \"Result\" : [], \"CreationTimestamp\" : \"Test timestamp\", \"ExpireTimestamp\" : \"Test expire\" }",
+                testQuestionMsg);
+        }
+
+        [TestMethod]
+        public void toJsonQuestionTestWithResponseOptions()
+        {
+            //Arrange
+            var rabbitMock = new Mock<IRabbitSubscriber>();
+            var controller = new RoomController(rabbitMock.Object);
+            var createdBy = "Test user";
+            var roomId = "Test room id";
+            var image = "Test image";
+            var questionText = "Test question text";
+            var responseOptions = "Response a,Response b,Response c";
+            string questionResult = null;
+            var CreationTimeStamp = "Test timestamp";
+            var ExpireTimeStamp = "Test expire";
+            var questionType = "Test Question Type";
+            string votes = null;
+
+
+            //Act
+            var testQuestionMsg = controller.toJsonQuestion(createdBy, roomId, image, questionText, responseOptions,
+                questionResult, CreationTimeStamp, ExpireTimeStamp, questionType, votes);
+
+            //Assert
+            Assert.AreEqual(
+                "{ \"_id\" : null, \"RoomId\" : \"Test room id\", \"CreatedById\" : \"Test user\", \"Votes\" : [], \"Img\" : \"Test image\", \"QuestionText\" : \"Test question text\", \"ResponseOptions\" : [{ \"Value\" : \"Response a\", \"Weight\" : 0 }, { \"Value\" : \"Response b\", \"Weight\" : 0 }, { \"Value\" : \"Response c\", \"Weight\" : 0 }], \"Result\" : [], \"CreationTimestamp\" : \"Test timestamp\", \"ExpireTimestamp\" : \"Test expire\" }",
+                testQuestionMsg);
+        }
+    }
+}
