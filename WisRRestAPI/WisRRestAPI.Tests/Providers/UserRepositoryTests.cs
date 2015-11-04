b78a184fd8d4274ffa82eade8d73b07e667dfe4e
Nikolaj Holden <nikolajholden@gmail.com>
2015-11-04 17:07:26 +0100

    Added getusersbyroomid

---
+++ WisRRestAPI/WisRRestAPI.Tests/Providers/UserRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WisRRestAPI.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WisRRestAPI.DomainModel.Tests
+{
+    [TestClass()]
+    public class UserRepositoryTests
+    {
+        [TestMethod()]
+        public void getUsersByRoomIdTest()
+        {
+            //Arrange
+            var _ur = new UserRepository(new dbHandler());
+           
+            
+            //Act
+             var result = _ur.getUsersByRoomId("5628e38cc7f5627fe84fbc45");
+            //Assert
+            Assert.AreNotEqual("[]",result);
+        }
+    }
+}
