31f7c810e975e17271cda695143425fab453c2ed
peheje <peterhj@gmail.com>
2015-11-11 13:13:00 +0100

    nullable latitute and longitude

--- WisRRestAPI/WisRRestAPI/DomainModel/Coordinate.cs
+++ WisRRestAPI/WisRRestAPI/DomainModel/Coordinate.cs
@@ -4,8 +4,8 @@ namespace WisR.DomainModels
 {
     public class Coordinate
     {
-        public Double Latitude { get; set; }
-        public Double Longitude { get; set; }
+        public Double? Latitude { get; set; }
+        public Double? Longitude { get; set; }
         public int AccuracyMeters { get; set; }
         public string FormattedAddress { get; set; }
         public string Timestamp { get; set; }
