34d40d2fd10b3c4eb6a9e9610ad7ccf041e3a414
Nikolaj Holden <nikolajholden@gmail.com>
2015-11-12 11:27:13 +0100

    Merged js files into one bundle

--- WisR/WisR/App_Start/BundleConfig.cs
+++ WisR/WisR/App_Start/BundleConfig.cs
@@ -8,8 +8,9 @@ namespace WisR
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/CryptoJS/rollups/sha512.js",
+                        "~/Scripts/modernizr-*",
                         "~/Scripts/jquery-{version}.js", 
                         "~/Scripts/WisRScripts/geolocationScripts.js", 
                         "~/Scripts/jquery.signalR-2.2.0.js",
@@ -23,12 +24,16 @@ namespace WisR
                         "~/Scripts/WisRScripts/AngularHomeController.js", 
                         "~/Scripts/WisRScripts/AngularFiltersScripts.js", 
                         "~/Scripts/WisRScripts/AngularRoomController.js",                     
-                        "~/Scripts/WisRScripts/Config.js"));
+                        "~/Scripts/WisRScripts/Config.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootstrap-filestyle.min.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/scrollglue.js",
+                        "~/Scripts/d3/d3.min.js",
+                        "~/Scripts/d3/d3pie.min.js"));
             
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
-            bundles.Add(new ScriptBundle("~/bundles/crypto").Include(
-                        "~/Scripts/CryptoJS/rollups/sha512.js"));
 
             var chartBundle = new ScriptBundle("~/bundles/chartbundle").Include(
                 "~/Scripts/chart.js", "~/Scripts/angular-chart.js");
@@ -39,16 +44,6 @@ namespace WisR
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap-filestyle.min.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/scrollglue.js",
-                      "~/Scripts/d3/d3.min.js",
-                      "~/Scripts/d3/d3pie.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
